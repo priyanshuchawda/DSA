c73dc36503715da0bb46f35e26a21dc2bd25dd4e
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-11-16 11:11:57 +0530

    ok

---
+++ pattern/10.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=1;
+    for (int j = 0; j < 5; j++)
+    {
+
+    
+    
+   for ( int i= 0; i < n; i++)
+   {
+    cout<<"*";
+   }
+   n++;
+   cout<<endl;
+    }
+
+    {
+        int n=5;
+         for (int j = 0; j < 5; j++)
+    {
+       
+    
+    
+   for ( int i= 0; i < n; i++)
+   {
+    cout<<"*";
+   }
+   n--;
+   cout<<endl;
+    }
+    }
+    return 0;
+}
---
+++ pattern/11.cpp
@@ -0,0 +1,16 @@
+// 1
+// 01
+// 101
+// 0101
+// 10101 
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        
+    }
+    
+    return 0;
+}
