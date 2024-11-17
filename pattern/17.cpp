f1c9213345e18e6ba7790e415d51fca3e6c868cb
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-11-17 06:40:19 +0530

    done

--- pattern/17.cpp
+++ pattern/17.cpp
@@ -1,38 +1,39 @@
 //    A
-//   ABA 
+//   ABA
 //  ABCBA
 // ABCDCBA
 
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n=1;
-    
-    char ch;
-    for (int i = 0; i <4; i++)
-    {   ch='A';
-        for (int j = 4; j>=n; j--)
-        {
-            cout<<" ";
+int main()
+{
+    int n = 1;
 
+    char ch;
+    for (int i = 0; i < 4; i++)
+    {
+        ch = 'A';
+        for (int j = 4; j >= n; j--)
+        {
+            cout << " ";
         }
         n++;
-        for (int k = 0; k < (2*i+1)/2 + 1; k++)
+        for (int k = 0; k < (2 * i + 1) / 2 + 1; k++)
         {
-            cout<<ch;
+            cout << ch;
             ch++;
         }
-        for (int h = 0; i < (2*i+1)/2; h++)
+        char temp=ch;
+        for (int h = 0; h < (2 * i + 1) / 2; h++)
         {
-            /* code */
+            cout << char(temp - 2);
+            temp--;
         }
         
-        
-        
-        
-        cout<<endl;
+
+        cout << endl;
     }
-    
+
     return 0;
 }
