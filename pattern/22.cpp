56a439077fefe9997b1151441a2e2e9a2aed7bd5
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-11-17 09:30:42 +0530

    done

--- pattern/22.cpp
+++ pattern/22.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 
 int main() {
-    int n;
-    for (int i = 0; i < n*2; i++)
-    {
-        
+    int n=4;
+    for (int i = 0; i < n*2 - 1; i++)
+    
+    {    
+        for (int j = 0; j < n*2 - 1; j++)
+        {
+            cout<<n;
+        }
+        cout<<endl;
     }
+
     
     return 0;
 }
