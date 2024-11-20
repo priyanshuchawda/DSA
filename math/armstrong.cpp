5788234421aa9c8e307b3b75a6d8d80815d5b6bf
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-11-20 14:18:27 +0530

    done

--- math/armstrong.cpp
+++ math/armstrong.cpp
@@ -1,33 +1,36 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include<math.h>
 using namespace std;
 
-int main() {
-    int n=1634;
-    int original=n;
-    int temp=0;
-    int c=0;
-    int sum=0;
+int main()
+{
+    int n = 1634;
+    int original = n;
+    int temp = 0;
+    int c = 0;
+    int sum = 0;
 
-    while (n!=0)
+    while (n != 0)
     {
-        temp=n%10;
+        temp = n % 10;
         c++;
-        n=n/10;
+        n = n / 10;
     }
-    n=original;
-    temp=0;
-    while (n!=0)
+    n = original;
+    temp = 0;
+    while (n != 0)
     {
-        temp=n%10;
-        sum=sum+pow(temp,c);
-        n=n/10;
+        temp = n % 10;
+        sum = sum + pow(temp, c);
+        n = n / 10;
     }
-    if (sum==original){
-        cout<<"Armstrong";
-
+    if (sum == original)
+    {
+        cout << "Armstrong";
     }
-    else{
-        cout<<"No";
+    else
+    {
+        cout << "No";
     }
     return 0;
 }
--- math/divisor.cpp
+++ math/divisor.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include<math.h>
+#include <math.h>
 using namespace std;
 
-int main() {
-    int n=36;
-    for (int i = 1; i*i <=n; i++)
+int main()
+{
+    int n = 36;
+    for (int i = 1; i * i <= n; i++)
     {
-        if (n%i==0)
+        if (n % i == 0)
         {
-            cout<<i<<endl;
-            if (n/i != i )
+            cout << i << endl;
+            if (n / i != i)
             {
-                cout<<n/i<<endl;
+                cout << n / i << endl;
             }
-            
         }
-        
     }
-    
+
     return 0;
 }
